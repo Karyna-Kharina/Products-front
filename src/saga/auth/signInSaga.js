5b8ee9a7c9679e5519c01971692956dd25a69d02
karinka <kharinakaryna@gmail.com>
2020-04-03 00:48:59 +0300

    fixed

--- src/saga/auth/signInSaga.js
+++ src/saga/auth/signInSaga.js
@@ -7,27 +7,29 @@ import {setMessageInfo} from "../../actions/info/infoAction";
 
 export function* signInSaga() {
 
-    const {email, password} = yield select(state => state.signIn);
+    try {
+        const {email, password} = yield select(state => state.signIn);
 
-    const result = yield call(
-        axios.get,
-        USERS_API + "/profile",
-        {
-            params: {
-                email,
-                password
+        const result = yield call(
+            axios.get,
+            USERS_API + "/profile",
+            {
+                params: {
+                    email,
+                    password
+                }
             }
-        }
-    );
+        );
 
-    if (!result.data) {
+        yield put(setMessageInfo({type: "success", text: "log ok"}));
+        yield put(setCurrentUser(result.data));
+        yield put({type: CLEAR_SIGN_IN});
+    } catch (e) {
         const type = "error";
         const text = "Your login or password are not valid!";
         yield put(setMessageInfo({type, text}));
     }
 
-    yield put(setCurrentUser(result.data));
-    yield put({type: CLEAR_SIGN_IN});
 }
 
 export function* watchSignInSaga() {
