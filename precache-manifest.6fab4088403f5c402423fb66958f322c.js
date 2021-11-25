d56849711562e78297edf4efe067a9c26f2d0a25
karyna <9548026git*>
2021-11-25 15:36:24 +0200

    Updates

---
+++ precache-manifest.6fab4088403f5c402423fb66958f322c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f765d3343743fb3ff8bcd1c81db19f",
+    "url": "/products/index.html"
+  },
+  {
+    "revision": "76e58dc8ca05b6697f92",
+    "url": "/products/static/js/2.535db1f5.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/products/static/js/2.535db1f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb800c51290dd5cf3c3",
+    "url": "/products/static/js/main.bce9a8e1.chunk.js"
+  },
+  {
+    "revision": "27e9173410e7524aede6",
+    "url": "/products/static/js/runtime-main.01a62100.js"
+  }
+]);
